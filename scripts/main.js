a08c00291980a8f8e58f13012a84f54f5112eb49
Kyle H <soulassassyn@gmail.com>
2023-04-08 14:07:47 -0600

    Category tabs and grid parts working

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 import { SpriteGrid } from "./spriteGrid.js";
 import { initializeSpriteGrid } from "./spriteGrid.js";
 import { MouseHoverEffect } from "./mouseHover.js";
-import { EscUI } from "./escUI.js";
 import { EnemySpawner } from './enemySpawner.js';
 
 runOnStartup(async runtime =>
@@ -19,11 +18,13 @@ async function OnBeforeProjectStart(runtime)
 	// instances are created and available to use here.
 
 	
-	initializeSpriteGrid(runtime, runtime.objects.rarityBox.name, 3, 3, 10);
-	initializeSpriteGrid(runtime, runtime.globalVars.gridName, 3, 3, 10);
+	initializeSpriteGrid(runtime, runtime.objects.rarityBox.name, 3, 1, 20);
+	initializeSpriteGrid(runtime, runtime.objects.gridShip.name, 3, 3, 20);
 	
 	// List of objects effected by the MouseHoverEffect class
-// 	const mouseHoverArrows = new MouseHoverEffect(runtime, "arrows", 2.25, 2, 0.1);
+	const mouseHoverShip = new MouseHoverEffect(runtime, "gridShip");
+	const mouseHoverWeapon = new MouseHoverEffect(runtime, "gridWeapon");
+	const mouseHoverEngine = new MouseHoverEffect(runtime, "gridEngine");
 	
 	// Enemy spawner
 	const enemySpawner = new EnemySpawner(runtime);
